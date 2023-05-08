ed36c6fbfdeebe1af4897ff03dc9364c95a6727e
simonegiancola09 <48059433+simonegiancola09@users.noreply.github.com>
2023-05-08 19:06:41 +0200

    started building agents, added seeds as global variables

--- src/global_configs.py
+++ src/global_configs.py
@@ -4,6 +4,8 @@ import numpy as np
 import random
 def init():
     global ROOT_DIR
+    global SEED
+    global NUMPY_SEED
     ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # as seed we set the year of our birth date
     SEED = random.seed(1999)
