1e23ed149872a54b2b094ca2562ef23aa4a72a74
darioliscai <liscai.d99@gmail.com>
2023-05-18 13:22:54 +0200

    Run ABM aggiornato con nuovo framework

--- src/run_ABM.py
+++ src/run_ABM.py
@@ -11,14 +11,34 @@ if __name__ == '__main__':
 
     df = pd.read_csv(r'..\data\raw\financial_US_NVDA_raw.csv')
     price = df['Close']
-    df_covid = pd.read_csv(r'..\data\raw\covid_US_raw.csv')
-    N = 200                 # num of nodes
-    M = 20                  # num of edges per node for cluster
-    P = 0.5                 # p of connection for erdos renyi and cluster graph
-    epochs = len(price)            # num of iterations
-    beta = 0.3
-    initial_stock_price = 60 # initial stock price
-    num_stocks = N * 20   # num of available stocks
+    # df_covid = pd.read_csv(r'..\data\raw\covid_US_raw.csv')
+
+    ## PARAMS OF THE MODEL ##
+    # number of nodes
+    N = 1000  
+    # num of edges per node for cluster          
+    M = 20      
+    # p of connection for erdos renyi and cluster graph            
+    P = 0.5     
+    # num of iterations            
+    epochs = 300   
+    # parameter controlling variance of price decision for random traders        
+    k = 0.0000002
+    # probability of being active (placing an order)
+    p = 0.1
+    # distribution over types of traders
+    prob_type = [0.97, 0.01, 0.01, 0.01]
+    # price value for fundamentalist traders 
+    # (set to None to automatically give mean price of price history)
+    p_f = None
+    ## paramenters on distribution of wealth
+    cash_low = 100
+    cash_high = 200
+    stocks_low = 100
+    stocks_high = 500
+
+
+
     ##########################################################
     ############### CHOICES FOR TOY MODELS #####################
     # below are different types of Rt to test our dynamics
@@ -42,17 +62,23 @@ if __name__ == '__main__':
 
     G = G_4
     Rt = Rt_null
-    print(Rt.shape)
 
     model = agents_construction.Nest_Model(
-                                        beta=beta, 
-                                        initial_stock_price = initial_stock_price,
-                                        external_var = Rt,
-                                        interaction_graph = G,
-                                        num_stocks = num_stocks)
-
+                                        k=k, 
+                                        price_history = list(price),
+                                        shock= Rt,
+                                        G = G,
+                                        p=p,
+                                        prob_type=prob_type,
+                                        p_f = p_f,
+                                        cash_low = cash_low,
+                                        cash_high = cash_high,
+                                        stocks_low = stocks_low,
+                                        stocks_high = stocks_high
+                                        )
+    print('Running ABM...')
     df_model, df_agents = agents_construction.run_model(model=model, epochs=epochs)
-    plt.plot(df_model['price'])
+    plt.plot(pd.concat([price, df_model['price']]).reset_index(drop=True))
     plt.show()
     # results = calibration.metropolis_hastings(model, 5, 0.01, std=0.1, true_data=price, burn_in=1)
 
